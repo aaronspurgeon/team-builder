85c91eb0c57e5b419d388c98c076f584346a2707
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-09 18:41:41 -0400

    added a default state with a list of team members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([
+    aaron = {
+      position: 'Front End Developer',
+      hobby: 'Video Games',
+      passion: 'Technology'
+    },
+    tom = {
+      position: 'Back End Developer',
+      hobby: 'Snowboarding',
+      passion: 'Quantum Physics'
+    },
+    sarah = {
+      position: 'Dev Ops Lead',
+      hobby: 'Sculpting',
+      passion: 'Local Politics'
+    }
+  ])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
     </div>
   );
 }
